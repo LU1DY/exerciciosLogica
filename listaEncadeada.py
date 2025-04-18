7c140f324a6c505451f2f78e3f67665935a22721
Luidy-Michael <luidymichael8@gmail.com>
2025-04-18 16:37:21 -0300

    mais funções simples

--- listaEncadeada.py
+++ listaEncadeada.py
@@ -43,6 +43,16 @@ class ListaEncadeada:
         else:
             novo_no.proximo = self.primeiro
             self.primeiro = novo_no
+
+    def adicionar_fim(self, valor):
+        novo_no = No(valor)
+        if self.primeiro is None:
+            self.primeiro = novo_no
+        else:
+            atual = self.primeiro
+            while atual.proximo is not None:
+                atual = atual.proximo
+            atual.proximo = novo_no
     
     def remover_inicio(self):
         atual = self.primeiro
@@ -52,6 +62,18 @@ class ListaEncadeada:
             self.primeiro = self.primeiro.proximo
             atual = None
 
+    def remover_fim(self):
+        atual = self.primeiro
+        if atual is None:
+            return print('Lista vazia')
+        else:
+            anterior = None
+            while atual.proximo is not None:
+                anterior = atual
+                atual = atual.proximo
+            anterior.proximo = None
+
+
 
 
 
@@ -60,13 +82,23 @@ class ListaEncadeada:
 listaEncadeada = ListaEncadeada()
 
 listaEncadeada.adicionar(10)
-listaEncadeada.adicionar(30)
 listaEncadeada.adicionar(20)
-listaEncadeada.adicionar_inicio(1)
-print(listaEncadeada.buscar(1))
-print('-------------')
+listaEncadeada.adicionar(30)
+listaEncadeada.adicionar(40)
+listaEncadeada.adicionar(50)
 listaEncadeada.imprimir()
-print('-------------')
+print('Adicionados os valores')
+
+retorno = listaEncadeada.buscar(10)
+print(retorno)
+print('Valor buscado')
+
+listaEncadeada.adicionar_inicio(5)
+listaEncadeada.adicionar_fim(55)
+listaEncadeada.imprimir()
+print('Valores adicionados no início e fim da lista')
+
 listaEncadeada.remover_inicio()
-print(listaEncadeada.buscar(1))
+listaEncadeada.remover_fim()
 listaEncadeada.imprimir()
+print('Valores removidos do início e fim da lista')
